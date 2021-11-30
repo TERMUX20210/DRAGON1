c4dded170e1fcd6ce6117c23817c9e9f0340371e
TERMUX20210 <94772724+TERMUX20210@users.noreply.github.com>
2021-11-30 22:00:52 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 clear
-chmod 777 *
-pkg install python
-pkg install figlet
-pkg install fish
-clear
-figlet AMIR-TERMUC20210
+read -p " Does you want to install DRAGON1 in Termux?(Yes/No) : " input
+
+if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
+    clear
+    cp bash.bashrc $HOME
+    cd /data/data/com.termux/files/usr/etc
+    rm -rf motd
+    rm -rf bash.bashrc
+    cd $HOME
+    mv bash.bashrc /data/data/com.termux/files/usr/etc
+    echo -e "\e[1;91mInstall Shod dadash"
+    echo -e "Restart Termux"
+    exit  3
+elif [[ $input == No || $input == no || $input == n || $input == N ]]; then
+exit 2
+else
+echo -e "\e[1;91mInvalid Option"
+exit 1
+fi
